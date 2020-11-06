ff92dde9d27b772d8073c62e90fd57bc832a34cf
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-06 16:23:17 +0100

    add. ts binary search

---
+++ src/typescript/binarySearch.ts
@@ -0,0 +1,25 @@
+const binarySearch = (sequence: number[], query: number): number => {
+  let low = 0
+  let high = sequence.length - 1
+  while (low <= high) {
+    let mid = (low + high) >> 2
+    if (sequence[mid] == query) {
+      return mid
+    } else if (sequence[mid] < query) {
+      low = mid + 1
+    } else {
+      high = mid - 1
+    }
+  }
+  return -1
+}
+
+
+const binarySearchTest = () => {
+  const seq = [2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65]
+  console.log(`Search result : ${binarySearch(seq, 7)}`)
+  console.log(`Missing query : ${binarySearch(seq, 6)}`)
+  console.log(`Empty sequence: ${binarySearch([], 7)}`)
+}
+
+binarySearchTest()
