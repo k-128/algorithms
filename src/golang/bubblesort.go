936ec640a1e639c5429ae0d2a6c2754fc9499157
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-06 18:42:36 +0100

    add. go bubble sort

---
+++ src/golang/bubblesort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func BubbleSort(sequence []int) []int {
+	var isSorted bool
+	for i := 0; i < len(sequence)-1; i++ {
+		isSorted = true
+		for j := 0; j < len(sequence)-1-i; j++ {
+			if sequence[j] > sequence[j+1] {
+				isSorted = false
+				sequence[j], sequence[j+1] = sequence[j+1], sequence[j]
+			}
+		}
+		if isSorted {
+			break
+		}
+	}
+	return sequence
+}
+
+func BubbleSortTest() {
+	seq := []int{16, 7, 9, 5, 65, 49, 37, 3, 28, 2, 21, 12, 4}
+	fmt.Printf("Initial sequence: %v\n", seq)
+	fmt.Printf("Sorted sequence : %v\n", BubbleSort(seq))
+}
