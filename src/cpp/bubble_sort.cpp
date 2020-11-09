02163875e83ff12846dba6295e1195207db80fac
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-09 11:01:12 +0100

    opt. cpp bubble sort

--- src/cpp/bubble_sort.cpp
+++ src/cpp/bubble_sort.cpp
@@ -2,34 +2,38 @@
 #include <vector>
 
 
-std::vector<int> BubbleSort(std::vector<int> sequence) {
-    for (unsigned int i = 0; i < sequence.size() - 1; i++) {
-        bool isSorted = true;
-        for (unsigned int j = 0; j < sequence.size() - 1 - i; j++) {
+void BubbleSort(std::vector<int>& sequence) {
+    for (unsigned int i = 0; i < sequence.size() - 1; ++i) {
+        bool is_sorted = true;
+        for (unsigned int j = 0; j < sequence.size() - 1 - i; ++j) {
             if (sequence[j] > sequence[j + 1]) {
-                isSorted = false;
+                is_sorted = false;
                 std::swap(sequence[j], sequence[j + 1]);
             }
         }
-        if (isSorted) {
+        if (is_sorted) {
             break;
         }
     }
-    return sequence;
+}
+
+
+void PrintSequence(std::vector<int> v) {
+    for (unsigned int i = 0; i < v.size(); ++i) {
+        std::cout << v.at(i) << " ";
+    }
 }
 
 
 int main() {
     std::vector<int> seq = {16, 7, 9, 5, 65, 49, 37, 3, 28, 2, 21, 12, 4};
-    std::vector<int> seq_sorted = BubbleSort(seq);
+    std::vector<int> seq_sorted(seq);
+    BubbleSort(seq_sorted);
+
     std::cout << "Initial sequence: ";
-    for (unsigned int i = 0; i < seq.size(); i++) {
-        std::cout << seq.at(i) << " ";
-    }
+    PrintSequence(seq);
     std::cout << "\nSorted sequence : ";
-    for (unsigned int i = 0; i < seq_sorted.size(); i++) {
-        std::cout << seq_sorted.at(i) << " ";
-    }
+    PrintSequence(seq_sorted);
     std::cout << "\n" << std::endl;
 
     return 0;
