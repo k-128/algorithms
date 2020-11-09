b45187bb8430416640093dfeaf68288ed69dc06c
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-09 11:02:25 +0100

    opt. cpp selection sort

--- src/cpp/selection_sort.cpp
+++ src/cpp/selection_sort.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 
 
-std::vector<int> SelectionSort(std::vector<int> sequence) {
-    for (unsigned int i = 0; i < sequence.size() - 1; i++) {
+void SelectionSort(std::vector<int>& sequence) {
+    for (unsigned int i = 0; i < sequence.size() - 1; ++i) {
         int i_lo_val = i + 1;
-        for (unsigned int j = 0; j < sequence.size() - 1 - i; j++) {
+        for (unsigned int j = 0; j < sequence.size() - 1 - i; ++j) {
             if (sequence[i_lo_val] > sequence[j + i + 1]) {
                 i_lo_val = j + i + 1;
             }
@@ -14,21 +14,25 @@ std::vector<int> SelectionSort(std::vector<int> sequence) {
             std::swap(sequence[i], sequence[i_lo_val]);
         }
     }
-    return sequence;
+}
+
+
+void PrintSequence(std::vector<int> v) {
+    for (unsigned int i = 0; i < v.size(); ++i) {
+        std::cout << v.at(i) << " ";
+    }
 }
 
 
 int main() {
     std::vector<int> seq = {16, 7, 9, 5, 65, 49, 37, 3, 28, 2, 21, 12, 4};
-    std::vector<int> seq_sorted = SelectionSort(seq);
+    std::vector<int> seq_sorted(seq);
+    SelectionSort(seq_sorted);
+
     std::cout << "Initial sequence: ";
-    for (unsigned int i = 0; i < seq.size(); i++) {
-        std::cout << seq.at(i) << " ";
-    }
+    PrintSequence(seq);
     std::cout << "\nSorted sequence : ";
-    for (unsigned int i = 0; i < seq_sorted.size(); i++) {
-        std::cout << seq_sorted.at(i) << " ";
-    }
+    PrintSequence(seq_sorted);
     std::cout << "\n" << std::endl;
 
     return 0;
