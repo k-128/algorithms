6484f3a95e0c2eb00b78496a1a602af0f84a957a
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-09 11:00:40 +0100

    opt. cpp binary search

--- src/cpp/binary_search.cpp
+++ src/cpp/binary_search.cpp
@@ -23,10 +23,10 @@ int BinarySearch(std::vector<int> sequence, int query) {
 
 int main() {
     std::vector<int> seq = {2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65};
+
     std::cout << "Search result : " << BinarySearch(seq, 7) << "\n";
     std::cout << "Missing query : " << BinarySearch(seq, 6) << "\n";
-    std::cout << "Empty sequence: " << BinarySearch({}, 7) << "\n"
-        << std::endl;
+    std::cout << "Empty sequence: " << BinarySearch({}, 7) << std::endl;
 
     return 0;
 }
