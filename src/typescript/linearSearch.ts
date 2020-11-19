330e42abac08de5a1200ef22240172f12b15dee5
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-19 01:14:23 +0100

    refmt. ts linearSearch

--- src/typescript/linearSearch.ts
+++ src/typescript/linearSearch.ts
@@ -1,19 +1,19 @@
 const linearSearch = (seq: number[], query: number): number => {
   for (let i = 0; i < seq.length; i++) {
     if (seq[i] == query) {
-      return i
+      return i;
     }
   }
-  return -1
+  return -1;
 }
 
 
 const linearSearchTest = () => {
-  const seq = [2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65]
+  const seq = [2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65];
 
-  console.log(`Search result : ${linearSearch(seq, 7)}`)
-  console.log(`Missing query : ${linearSearch(seq, 6)}`)
-  console.log(`Empty sequence: ${linearSearch([], 7)}`)
+  console.log(`Search result : ${linearSearch(seq, 7)}`);
+  console.log(`Missing query : ${linearSearch(seq, 6)}`);
+  console.log(`Empty sequence: ${linearSearch([], 7)}`);
 }
 
-linearSearchTest()
+linearSearchTest();
