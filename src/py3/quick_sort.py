88a47b8c53c0900fce766791a93c80ed6254a16b
xnr-k <22292600+xnr-k@users.noreply.github.com>
2020-11-21 20:13:28 +0100

    upd. py3 sort tests

--- src/py3/quick_sort.py
+++ src/py3/quick_sort.py
@@ -24,12 +24,35 @@ def quick_sort(seq: List[Any]) -> None:
 
 
 if __name__ == "__main__":
-    seq_1 = [16, 7, 9, 5, 65, 49, 37, 3, 28, 2, 21, 12, 4]
-    seq_1_sorted = seq_1.copy()
-    seq_2 = ["p", "g", "i", "j", "65", "W", "K", "c", "B", "b", "21", "l", "d"]
-    seq_2_sorted = seq_2.copy()
-    quick_sort(seq_1_sorted)
-    quick_sort(seq_2_sorted)
+    sorting_fn = quick_sort
 
-    print(f"seq_1: {seq_1}\nseq_1_sorted: {seq_1_sorted}")
-    print(f"seq_2: {seq_2}\nseq_2_sorted: {seq_2_sorted}")
+    def is_sorted(seq: List[Any]) -> bool:
+        for i in range(len(seq)-1):
+            if seq[i] > seq[i+1]:
+                return False
+        return True
+
+    # empty
+    seq = []
+    sorting_fn(seq)
+    print(f"test_empty: {'passed' if is_sorted(seq) else 'failed'}.")
+
+    # +
+    seq = [16, 7, 9, 5, 65, 49, 37, 3, 28, 2, 21, 12, 4]
+    sorting_fn(seq)
+    print(f"test_int_1: {'passed' if is_sorted(seq) else 'failed'}.")
+
+    # +, -
+    seq = [-16, 7, -9, 5, -65, 49, -37, 3, -28, 2, -21, 12, -4]
+    sorting_fn(seq)
+    print(f"test_int_2: {'passed' if is_sorted(seq) else 'failed'}.")
+
+    # +, -, =
+    seq = [1, 2, -4, -7, 1, 9, -7, 2, 2, 6, 1, 12, 4]
+    sorting_fn(seq)
+    print(f"test_int_3: {'passed' if is_sorted(seq) else 'failed'}.")
+
+    # string
+    seq = ["p", "g", "i", "j", "65", "W", "K", "c", "B", "b", "21", "l", "d"]
+    sorting_fn(seq)
+    print(f"test_str: {'passed' if is_sorted(seq) else 'failed'}.")
